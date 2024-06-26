9005c2e28292a213f621153d243a031f94d3f506
profeclipse <davehunt@me.com>
2024-06-25 21:04:21 -0400

    Changed bufferline to buffer mode. Commented out auto-session.

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,33 +1,37 @@
 return {
-  "goolord/alpha-nvim",
-  event = "VimEnter",
-  config = function()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.dashboard")
+	"goolord/alpha-nvim",
+	event = "VimEnter",
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
 
-    dashboard.section.header.val = {
-      "                                                     ",
-      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-      "                                                     ",
-    }
+		dashboard.section.header.val = {
+			"                                                     ",
+			"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+			"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+			"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+			"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+			"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+			"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+			"                                                     ",
+		}
 
-    dashboard.section.buttons.val = {
-      dashboard.button("n", "  > New File", "<cmd>ene<cr>"),
-      dashboard.button("c", "  > NVIM config", "<cmd>Neotree toggle " .. vim.call("stdpath","config") .. "<cr>"),
-      dashboard.button("e", "  > Toggle file explorer", "<cmd>Neotree toggle<cr>"),
-      dashboard.button("f", "  > Fuzzy find file", "<cmd>Telescope find_files<cr>"),
-      dashboard.button("s", "  > Search word", "<cmd>Telescope live_grep<cr>"),
-      dashboard.button("r", "  > Restore Session for cwd", "<cmd>SessionRestore<cr>"),
-      dashboard.button("q", "  > Quit NVIM", "<cmd>qa<cr>"),
-    }
+		dashboard.section.buttons.val = {
+			dashboard.button("n", "  > New File", "<cmd>ene<cr>"),
+			dashboard.button(
+				"c",
+				"  > NVIM config",
+				"<cmd>Neotree toggle " .. vim.call("stdpath", "config") .. "<cr>"
+			),
+			dashboard.button("e", "  > Toggle file explorer", "<cmd>Neotree toggle<cr>"),
+			dashboard.button("f", "  > Fuzzy find file", "<cmd>Telescope find_files<cr>"),
+			dashboard.button("s", "  > Search word", "<cmd>Telescope live_grep<cr>"),
+			dashboard.button("r", "  > Restore Session for cwd", "<cmd>SessionRestore<cr>"),
+			dashboard.button("q", "  > Quit NVIM", "<cmd>qa<cr>"),
+		}
 
-    alpha.setup(dashboard.opts)
+		alpha.setup(dashboard.opts)
 
-    vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
-  end,
+		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+	end,
 }
